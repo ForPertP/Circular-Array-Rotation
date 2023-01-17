fac327da2bc60a3a38bf83c9a1aed1b1c18e5958
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-01-17 10:27:03 +0900

    Update Circular-Array-Rotation.cpp

--- Circular-Array-Rotation.cpp
+++ Circular-Array-Rotation.cpp
@@ -1,3 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'circularArrayRotation' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER k
+ *  3. INTEGER_ARRAY queries
+ */
+
 vector<int> circularArrayRotation(vector<int> a, int k, vector<int> queries)
 {
     int n = a.size();
